e0d9e7c5096840ac2dbc312a409ebed7ac2e80c1
Nguyen Ngoc Gia Bao <nguyenngocgiabao68@gmail.com>
2026-03-27 20:31:07 +0700

    Hoan thanh BTH 7: Hien thi danh sach voi ListView

--- BTH 7/app/src/main/java/till/edu/baith7_listview/MainActivity.java
+++ BTH 7/app/src/main/java/till/edu/baith7_listview/MainActivity.java
@@ -1,17 +1,18 @@
 package till.edu.baith7_listview;
 
-// NHỚ GIỮ NGUYÊN DÒNG PACKAGE CỦA BẠN Ở ĐÂY NHÉ!
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lvNgonNgu;
-    ArrayList<String> dsNgonNgu;
+    ListView lvTinhThanh;
+    ArrayList<String> dsTinhThanh;
     ArrayAdapter<String> adapter;
 
     @Override
@@ -19,28 +20,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lvNgonNgu = findViewById(R.id.lvNgonNgu);
+        lvTinhThanh = findViewById(R.id.lvTinhThanh);
 
-        dsNgonNgu = new ArrayList<>();
-        dsNgonNgu.add("Java (Android)");
-        dsNgonNgu.add("Python");
-        dsNgonNgu.add("C++");
-        dsNgonNgu.add("C#");
-        dsNgonNgu.add("JavaScript");
-        dsNgonNgu.add("Kotlin");
-        dsNgonNgu.add("Swift (iOS)");
-        dsNgonNgu.add("PHP");
-        dsNgonNgu.add("Ruby");
-        dsNgonNgu.add("Dart (Flutter)");
-        dsNgonNgu.add("Go");
-        dsNgonNgu.add("Rust");
+        dsTinhThanh = new ArrayList<>();
+        dsTinhThanh.add("Hà Nội");
+        dsTinhThanh.add("Thành phố Hồ Chí Minh");
+        dsTinhThanh.add("Đồng Nai");
+        dsTinhThanh.add("Bình Thuận");
+        dsTinhThanh.add("Ninh Thuận");
+        dsTinhThanh.add("Nha Trang");
 
-        adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                dsNgonNgu
-        );
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dsTinhThanh);
+        lvTinhThanh.setAdapter(adapter);
 
-        lvNgonNgu.setAdapter(adapter);
+        lvTinhThanh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String tenTinh = dsTinhThanh.get(position);
+
+                Toast.makeText(MainActivity.this, "Bạn đã chọn: " + tenTinh, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
