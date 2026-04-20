8f65c1104060217101131f7f3743c3ca22e25115
Nguyen Ngoc Gia Bao <nguyenngocgiabao68@gmail.com>
2026-04-20 08:06:32 +0700

    LẦN THỨ 3

--- thigk2.nguyenngocgiabao/app/src/main/java/till/edu/thigk2nguyenngocgiabao/ChucNang2Activity.java
+++ thigk2.nguyenngocgiabao/app/src/main/java/till/edu/thigk2nguyenngocgiabao/ChucNang2Activity.java
@@ -1,24 +1,33 @@
 package till.edu.thigk2nguyenngocgiabao;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ChucNang2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chuc_nang2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ListView lvCaKhuc = findViewById(R.id.lvCaKhuc);
+
+        String[] danhSachCaKhuc = {
+                "Tiến quân ca",
+                "Chào em cô gái Lam Hồng",
+                "Trường Sơn Đông Trường Sơn Tây",
+                "Cô gái mở đường",
+                "Bác đang cùng chúng cháu hành quân",
+                "Chiếc gậy Trường Sơn",
+                "Lên đàng",
+                "Mùa xuân trên Thành phố Hồ Chí Minh",
+                "Giai điệu Tổ quốc",
+                "NGUYỄN NGỌC GIA BẢO"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, danhSachCaKhuc);
+        lvCaKhuc.setAdapter(adapter);
     }
 }
